9622416dec384601312386fbe0c280e0cdaf59d4
Illya <illya123456@outlook.com>
2021-04-02 22:52:11 +0300

    fixed style

--- homework_3/practice.py
+++ homework_3/practice.py
@@ -1,16 +1,23 @@
 import datetime
 import requests
+
+
 class ErrorTimesOfYear(Exception):
     pass
+
+
 class HenHouse:
     min_hens_accepted = 5
-    hens_productivity = {'winter': 0.25, 'spring': 0.75, 'autumn': 0.5, 'summer': 1}
+    hens_productivity = {'winter': 0.25, 'spring': 0.75, 'autumn': 0.5,
+                         'summer': 1}
+
     def __init__(self, hen_count: int):
         self.hen_count = hen_count
         if self.hen_count < self.min_hens_accepted:
             raise ValueError('You need more hens))')
         else:
             print("You have enough hens!!!")
+
     @property
     def season(self) -> str:
         month = datetime.date.month
@@ -25,14 +32,18 @@ class HenHouse:
             return "autumn"
         else:
             return "winter"
+
     def _productivity_index(self):
         if self.season in self.hens_productivity:
             return self.hens_productivity[self.season]
         raise ErrorTimesOfYear
+
     def get_eggs_daily(self, hen_count: int) -> int:
         return int(hen_count * self._productivity_index())
+
     def get_max_count_for_soup(self, expected_eggs: int) -> int:
-        # based on hen_count self and get_eggs_daily we need to count hew many hens we can kill
+        # based on hen_count self and get_eggs_daily we need to count hew
+        # many hens we can kill
         # but still get expected_eggs count of eggs
         if self.hen_count < self.min_hens_accepted \
                 or self.get_eggs_daily(self.hen_count) < expected_eggs:
@@ -40,6 +51,7 @@ class HenHouse:
         else:
             return int((self.get_eggs_daily(self.hen_count) - expected_eggs)
                        / self.hens_productivity[self.season])
+
     @staticmethod
     def food_price(self) -> int:
         page = requests.get("http:/chicken/food")
